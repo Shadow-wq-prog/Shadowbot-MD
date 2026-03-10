8a03db29f5a9d462d37f6b821eaa85ec5cfd98c9
Shadow-wq-prog <johangalindezmogollonjosue@gmail.com>
2026-03-10 12:10:21 -0500

    Eliminar owner.js

--- owner.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    command: ['owner', 'creador'],
-    run: async (sock, m, from) => {
-        await sock.sendMessage(from, { text: '😎 *Dueño:* Fernando\n*GitHub:* Shadow-wq-prog' });
-    }
-};
